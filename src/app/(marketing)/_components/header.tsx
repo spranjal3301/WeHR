46d0eee5f9d01ae06c28e2f4d4d24f8c5091a1aa
Pranjal <spranjal394@gmail.com>
2025-03-01 15:44:16 +0530

    -

--- src/app/(marketing)/_components/header.tsx
+++ src/app/(marketing)/_components/header.tsx
@@ -1,9 +1,12 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useSidebar } from "@/components/ui/sidebar";
 import { useIsTab } from "@/hooks/use-tab";
+import Bell from "@/icons/bell";
+import Dp from "@/icons/dp";
 import Menuicon from "@/icons/menu-icon";
+import Message from "@/icons/message";
 import { IconMenu2 } from "@tabler/icons-react";
-import { Bell, ChevronDown, MessageSquare, Search } from "lucide-react";
+import { ChevronDown, MessageSquare, Search } from "lucide-react";
 import React from "react";
 
 // type Props = {};
@@ -22,31 +25,27 @@ const Header = () => {
           <input
             type="text"
             placeholder="Search"
-            className="pl-10 pr-4 py-2 border border-[#e0e0e0] rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-[#3786f1] w-[240px]"
+            className="pl-10 pr-4 py-2 border border-[#e0e0e0] rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-[#ff5151] w-[300px]"
           />
         </div>
       </div>
 
-      <div className="flex items-center space-x-4">
-        <button className="relative">
-          <Bell className="h-5 w-5 text-[#686868]" />
-          <span className="absolute -top-1 -right-1 h-4 w-4 bg-[#ff5151] rounded-full flex items-center justify-center text-[10px] text-white">
-            2
-          </span>
+      <div className="flex items-center px-4 space-x-6 md:space-x-8">
+        <button className="cursor-pointer">
+          <Bell/>
         </button>
 
-        <button>
-          <MessageSquare className="h-5 w-5 text-[#686868]" />
+        <button className="cursor-pointer">
+          <Message/>
         </button>
 
         <div className="flex items-center">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="/" alt="AJ" />
-            <AvatarFallback>AJ</AvatarFallback>
-          </Avatar>
+          {/* <Avatar className="h-8 w-8"> */}
+             <Dp/>
+          {/* </Avatar> */}
 
           <div className="ml-2 flex items-center">
-            <span className="text-sm font-medium hidden lg:block">Admin John</span>
+            <span className="text-sm font-medium hidden lg:block">Admirra John</span>
             <ChevronDown className="ml-1 h-4 w-4 text-[#686868]" />
           </div>
         </div>
